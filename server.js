2be928edcdfa1fbfe666a7c5b875ae9bfa16b607
Arnel <aarnelhernandez@gmail.com>
2020-02-12 22:32:59 -0600

    Deploy

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ const products = require('./routes/api/products')
 const cartItems = require('./routes/api/cart')
 
 //USE ROUTES
-app.use('api/processors', products)
-app.use('api/processors/:id', products)
-app.use('api/cart', cartItems)
+app.use('/api/processors', products)
+app.use('/api/processors/:id', products)
+app.use('/api/cart', cartItems)
 
 //Handle Production
 if(process.env.NODE_ENV === 'production'){
