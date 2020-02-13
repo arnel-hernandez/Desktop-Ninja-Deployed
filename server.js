31c79cc944a43b6547d13151568d5775c0077aeb
Arnel <aarnelhernandez@gmail.com>
2020-02-12 20:50:23 -0600

    Deployment

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
 
-// app.get('*', (request, response) => {
-// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 //TO DEPLOY
 const port = process.env.PORT || 5000
