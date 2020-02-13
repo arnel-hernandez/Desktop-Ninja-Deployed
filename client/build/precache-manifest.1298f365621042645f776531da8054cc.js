bceaf6fc6882453c09409d40cece1afdfe516d3c
Arnel <aarnelhernandez@gmail.com>
2020-02-12 23:01:29 -0600

    Fix Server

---
+++ client/build/precache-manifest.1298f365621042645f776531da8054cc.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4087e9e574c22d13580bf9c42f317066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc52a96af7183ea47ef1",
+    "url": "/static/css/2.2ec3db0d.chunk.css"
+  },
+  {
+    "revision": "10362025c747930a7240",
+    "url": "/static/css/main.fb42b848.chunk.css"
+  },
+  {
+    "revision": "fc52a96af7183ea47ef1",
+    "url": "/static/js/2.b636a24b.chunk.js"
+  },
+  {
+    "revision": "d97ae3a33e14a7069604081c454560c6",
+    "url": "/static/js/2.b636a24b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10362025c747930a7240",
+    "url": "/static/js/main.65567e27.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "9c02cd3c9b9fdf7bb76504cd2cc318f0",
+    "url": "/static/media/Corei3.9c02cd3c.jpg"
+  },
+  {
+    "revision": "a8a18959fd7036bdb173726f9742c692",
+    "url": "/static/media/Corei3.a8a18959.png"
+  },
+  {
+    "revision": "c115d9cbe9b3e899c3ff4c23327716b5",
+    "url": "/static/media/Corei5.c115d9cb.png"
+  },
+  {
+    "revision": "e63857f5447c8dfeb2eb7e48921b5b10",
+    "url": "/static/media/Corei5.e63857f5.jpg"
+  },
+  {
+    "revision": "017f488087b166ba717b51377e18333c",
+    "url": "/static/media/Corei7.017f4880.png"
+  },
+  {
+    "revision": "7321d2fc3ade54fd8a5a1642910f59bc",
+    "url": "/static/media/Corei7.7321d2fc.jpg"
+  },
+  {
+    "revision": "c7bc6f63d470b4139a286c178dac34b2",
+    "url": "/static/media/Corei9.c7bc6f63.jpg"
+  },
+  {
+    "revision": "38e95debfedac624c2de2d2758a00764",
+    "url": "/static/media/Ryzen3.38e95deb.jpg"
+  },
+  {
+    "revision": "c57d622b7ba8fe476fa1c4fe72a99c1c",
+    "url": "/static/media/Ryzen3.c57d622b.png"
+  },
+  {
+    "revision": "2c24ac167abd0ee44175b2525affc4e0",
+    "url": "/static/media/Ryzen5.2c24ac16.png"
+  },
+  {
+    "revision": "a56e750cbc936e3315052bc5f9955ac0",
+    "url": "/static/media/Ryzen5.a56e750c.jpg"
+  },
+  {
+    "revision": "bd4a678db4acfe17470035e01dc1c581",
+    "url": "/static/media/Ryzen7.bd4a678d.png"
+  },
+  {
+    "revision": "d699802c4e8b94d46daf99f098e585f0",
+    "url": "/static/media/Ryzen7.d699802c.jpg"
+  }
+]);
