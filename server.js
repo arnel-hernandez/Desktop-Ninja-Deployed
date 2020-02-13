1262d34cbcae295c45cb22e0a389bcaa87831ff5
Arnel <aarnelhernandez@gmail.com>
2020-02-12 22:09:19 -0600

    Deploy

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use('/cart', cartItems)
 //Handle Production
 if(process.env.NODE_ENV === 'production'){
   //Static folder
-  app.use(express.static(__dirname + '/public/'));
+  app.use(express.static(__dirname + '/client/build/static'));
 
   //Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/client/build/index.html'));
 }
 
 //TO DEPLOY
